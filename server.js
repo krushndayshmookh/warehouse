37a3fb1ce6178d54520041291ea3beda8aaa64b0
Krushn Dayshmookh <krushndayshmookh@gmail.com>
2020-11-05 07:39:58 +0000

    haha. rokkie mistake haha

--- server.js
+++ server.js
@@ -3,8 +3,8 @@ const PORT = process.env.PORT || 3000
 
 const app = express()
 
-app.use(express.static(__dirname + "/public")
+app.use(express.static(__dirname + "/public"))
 
 app.listen(PORT, err => {
   console.log("Listening on port " + PORT + "...")
-}
+})
